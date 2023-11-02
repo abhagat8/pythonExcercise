36ffe6ab911d7be9977aae0af2247e1517d3b0eb
Abhi Bhagat <145463258+abhagat8@users.noreply.github.com>
2023-11-02 19:59:45 +0000

    search movies by actor commit

--- query.py
+++ query.py
@@ -1,7 +1,6 @@
 from movies import Movies
 movies = Movies('./movies.txt')
 
-
 print("menu:")
 print("sn: Search movie name")
 print("sc: Search cast name")
@@ -13,14 +12,26 @@ def list():
     for i in range (0, len(movies._movies), 1):
         print(movies._movies[i]['name'])
 
-def iscast():
-
-
 def ismoviename():
     checkname=input("enter the movie name: ").lower()
     for i in range(0, len(movies._movies), 1):
         if(movies._movies[i]['name'].lower().__contains__(checkname.lower())):
             print(movies._movies[i]['name'])
+
+def iscast():
+    actor=input("Enter the actor/actress name: ").lower()
+    for i in range(0, len(movies._movies), 1):
+        castlength=len(movies._movies[i]['cast'])
+        castmembers=[]
+
+        for j in range(0, castlength, 1):
+
+            if actor in movies._movies[i]['cast'][j].lower():
+                castmembers.append(movies._movies[i]['cast'][j])
+
+        if(len(castmembers)!=0):
+            print(movies._movies[i]['name'])
+            print(castmembers)
 
 x=True
 while x:
